e3e95b703d793ed2e03799ac9ce202f9329f2ac5
TBU <tbu@smartequipl.com>
2020-10-01 14:32:50 -0400

    jira-001: Adding PropertyConfig for basic crawling external cli command

---
+++ src/main/java/com/commontools/http/webcrawler/config/PropertyConfig.java
@@ -0,0 +1,24 @@
+package com.commontools.http.webcrawler.config;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * @author tbu
+ */
+@Component
+@NoArgsConstructor
+@Data
+public class PropertyConfig {
+
+    @Value("${followRedirects}")
+    public Boolean followRedirects;
+
+    @Value("${timeout}")
+    public Integer timeout;
+
+
+}
